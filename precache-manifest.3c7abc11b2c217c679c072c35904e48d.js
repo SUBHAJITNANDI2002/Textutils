b442882d9a697000df4be6abf165978a9a083209
Subhajit Nandi <nandisubhajit2002@gmail.com>
2022-09-10 11:47:22 +0530

    Updates

---
+++ precache-manifest.3c7abc11b2c217c679c072c35904e48d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bac28bcdc6c7af33b70c",
+    "url": "/Textutils/static/css/main.d429c360.chunk.css"
+  },
+  {
+    "revision": "bac28bcdc6c7af33b70c",
+    "url": "/Textutils/static/js/main.7b3005e4.chunk.js"
+  },
+  {
+    "revision": "aa3a03254ad7f842b4a3",
+    "url": "/Textutils/static/js/runtime~main.a0c43713.js"
+  },
+  {
+    "revision": "4cfe2348016058b1c2d0",
+    "url": "/Textutils/static/js/2.c1be3fed.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/Textutils/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "05182d7605f22ce43afd8e6a5abaceaf",
+    "url": "/Textutils/index.html"
+  }
+];
